29dc955fc6a79d781e5edc309904818264c61500
DavidCuturic5 <david.cuturic5@gmail.com>
2024-12-08 19:11:57 +0100

    Add files via upload

---
+++ ocjenskaVjezba1.PolugodistePetlje01/ocjenskaVjezba1.PolugodistePetlje01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ocjenskaVjezba1.PolugodistePetlje01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int godina = 1900; godina <= 2100; godina++)
+            {
+                if ((godina % 400 == 0) || (godina % 4 == 0 && godina % 100 != 0)) {
+                    Console.WriteLine(godina);
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
